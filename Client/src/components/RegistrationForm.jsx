3f9114c40a1944545b25d8dd05de73dbae0e5493
Eshaan Bahuguna <eshaanbahuguna.official@gmail.com>
2023-01-20 21:42:33 +0530

    Added Validation for RegistrationForm
    - checkbox validation and styling needs to be implemented for login &
    registration page

--- Client/src/components/RegistrationForm.jsx
+++ Client/src/components/RegistrationForm.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import {Input, Button, Colors} from '../ui/ui';
+import {Input, Button, Colors, MessageBox} from '../ui/ui';
 
 export default function RegistrationForm(){
+    // Form Handling
     const [registraionFormData, setRegistrationFormData] = useState({
         email: '', 
         registrationNumber: '', 
         password: '', 
         confirmPassword: ''
     })
+
+    //Show Message/Alert Box
+    const [showMessageBox, setMessageBox] = useState({
+        text: '', 
+        display: false
+    }); 
+
+    function clearMessageBox(){
+        setMessageBox({
+            text: '', 
+            display: false
+        })
+    }
 
     function setFormData(name, value){
         setRegistrationFormData(prevData => {
@@ -19,48 +33,110 @@ export default function RegistrationForm(){
         })
     }
 
-    function onSubmitRegistrationForm(){
-        console.table(registraionFormData);
+    function checkRegistrationNumber(number){
+        const   checkDigits = /^\d{2}\w{3}\d{5}$/, 
+                testOne = checkDigits.test(number),
+                checkLetters = /[A-Z][A-Z][A-Z]/,
+                testTwo = checkLetters.test(number.substring(2, 5));
+
+        if(number.length === 10 && testOne && testTwo){
+            return true;
+        }
+
+        return false;
     }
 
-    return (
-        <div className="form-wrapper card" style={{paddingBottom: '5rem'}}>
-             <Input 
-                labelText='Email' 
-                placeholder='Enter your email address' 
-                type='email' 
-                name='email'
-                value={registraionFormData.email}
-                handleChange={setFormData}
-            />        
-             <Input 
-                labelText='Registration Number' 
-                placeholder='Enter your registration number' 
-                type='text' 
-                name='registrationNumber'
-                value={registraionFormData.registrationNumber}
-                handleChange={setFormData}
-            />        
-             <Input 
-                labelText='Create Password'  
-                type='password' 
-                name='password'
-                value={registraionFormData.password}
-                handleChange={setFormData}
-            />        
-             <Input 
-                labelText='Confirm Password' 
-                type='password' 
-                name='confirmPassword'
-                value={registraionFormData.confirmPassword}
-                handleChange={setFormData}
-            />     
+    function onSubmitRegistrationForm(){
+        console.table(registraionFormData);
 
-            <Button 
-                color={Colors.primaryColor} 
-                hoverColor={Colors.primaryColorDark} 
-                onButtonPress={onSubmitRegistrationForm}
-            > Register </Button>
+        // Registration Form Validation 
+        const emailValidation = /@vitbhopal.ac.in$/
+        let message = 'Account Successfully created!';
+
+        const   validPassword = registraionFormData.password.length !== 0,
+                samePassword = registraionFormData.password === registraionFormData.confirmPassword,
+                validEmail = emailValidation.test(registraionFormData.email), 
+                validRegistrationNumber = checkRegistrationNumber(registraionFormData.registrationNumber);
+
+        if(!validPassword){
+            message = 'Entered Password is Invalid';
+        }        
+        if(!samePassword){
+            message = 'Password do not match';
+        }
+        if(!validEmail){
+            message = 'Entered Email Id is Invalid';
+        }
+        if(!validRegistrationNumber){
+            message = 'Entered Registration Number is incorrect';
+        }
+
+        // Show & Clear message box
+        setMessageBox({
+            display: true, 
+            text: message
+        })
+        setTimeout(()=>{
+            clearMessageBox();
+        }, 3000);
+    }
+
+    const divStyle = {
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        paddingTop: '13rem',
+    }
+
+
+    return (
+        <div style={divStyle}>
+            {showMessageBox.display && <MessageBox>{showMessageBox.text}</MessageBox>}
+
+            <div className="form-wrapper card" style={{paddingBottom: '5rem'}}>
+                <Input 
+                    labelText='Email' 
+                    placeholder='Enter your email address' 
+                    type='email' 
+                    name='email'
+                    value={registraionFormData.email}
+                    handleChange={setFormData}
+                />        
+                <Input 
+                    labelText='Registration Number' 
+                    placeholder='Enter your registration number' 
+                    type='text' 
+                    name='registrationNumber'
+                    value={registraionFormData.registrationNumber}
+                    handleChange={setFormData}
+                />        
+                <Input 
+                    labelText='Create Password'  
+                    type='password' 
+                    name='password'
+                    value={registraionFormData.password}
+                    handleChange={setFormData}
+                />        
+                <Input 
+                    labelText='Confirm Password' 
+                    type='password' 
+                    name='confirmPassword'
+                    value={registraionFormData.confirmPassword}
+                    handleChange={setFormData}
+                />     
+
+                <div style={{display: 'flex', justifyContent: 'space-between', padding: '0 1rem', gap: '1rem'}}>
+                    <input type='checkbox' style={{display: 'inline-block', marginBottom: '1.3rem'}} /> 
+                    <p style={{display: 'inline-block'}}>By creating an Account, I agree to the terms and conditions & privacy policy</p>
+                </div>
+
+
+                <Button 
+                    color={Colors.primaryColor} 
+                    hoverColor={Colors.primaryColorDark} 
+                    onButtonPress={onSubmitRegistrationForm}
+                > Register </Button>
+            </div>
         </div>
     )
 }
